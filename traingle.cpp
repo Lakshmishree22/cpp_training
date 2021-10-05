ee69a4b7eca3d321ea1e5e93b7230cea6798ef11
lakshmi-shree-dr <lakshmi-shree.dr@hpe.com>
2021-10-05 10:09:23 +0530

    firstcommit

---
+++ traingle.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+struct Triangle{ 
+
+double s1,s2,s3; 
+}; 
+
+
+int main(int argc, char const *argv[])
+{
+ double s1=3,s2=4,s3=12;
+
+    if(s1+s2>s3 &&   s2+s3>s1 && s3+s1>s2){
+        cout<<"is a valid traingle="<<sizeof(Triangle)<<endl;
+    }
+    else{
+        cout<<"is not valid traingle="<<sizeof(Triangle)<<endl;
+        return 0;
+    }
+
+
+    Triangle t3={s1,s2,s3};
+
+    return 0;
+}
