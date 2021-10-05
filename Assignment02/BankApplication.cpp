aa4169a53c2e4c3c708e38272eacd4b668881585
lakshmi-shree-dr <lakshmi-shree.dr@hpe.com>
2021-10-05 22:03:58 +0530

    Assignment_Classbasics

--- Assignment02/BankApplication.cpp
+++ Assignment02/BankApplication.cpp
@@ -16,31 +16,30 @@ using namespace std;
     }
 
     int BankAccount::deposit(double amt){
-        if(amt<0){
-           return -1;
-        }
-        else if(Balance<amt){
-            return 1;
+        if(amt<0.0){
+           std::cout<<"Cannot deposit a negative value"<<std::endl;
         }
         else{
             Balance+=amt;
-            return 0;
+            std::cout<<"Deposit successfull!! Available Balance="<<Balance<<std::endl;
         }
     }
 
    int BankAccount::withdraw(double amt){
        if(amt<0){
-           return -1;
+           std::cout<<"Cannot withdraw a negative value"<<std::endl;
        }
        else if(amt<Balance){
            Balance=Balance-amt;
+           std::cout<<"Withdraw successfull!! Available Balance="<<Balance<<std::endl;
            return 0;
        }
    }
    
    void BankAccount::creditInterest(){
        if(Balance>0){
-           Balance+=(Balance*InterestRate/1200);
+           Balance=Balance+(Balance*(InterestRate/1200));
+           std::cout<<"Interest credited!! Available Balance="<<Balance<<std::endl;
        }
    }
    void BankAccount::show(){
@@ -53,10 +52,11 @@ using namespace std;
    int BankAccount::changeName(string FirstName,string LastName){
 
        if(this->LastName.compare(LastName)==0){
-            this->LastName=LastName;
+            this->FirstName=FirstName;
+            std::cout<<"First name is changed to "<<FirstName<<std::endl;
        }
        else{
-           return -1;
+          std::cout<<"Ensure to input proper last name"<<std::endl;
        }
-        this->FirstName=FirstName;
+
    }
--- Assignment02/app.cpp
+++ Assignment02/app.cpp
@@ -7,4 +7,15 @@ int main(int argc, char const *argv[])
 {
     BankAccount b1("Lakshmi","shree","$123",22,200.0,10.0);
     b1.show();
+
+    b1.deposit(20);
+    b1.deposit(-1);
+
+    b1.withdraw(10);
+    b1.withdraw(-1);
+
+    b1.creditInterest();
+
+    b1.changeName("shweta","shree");
+    b1.changeName("nag","sudha");
 }
